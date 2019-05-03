38506e3114a3c1925e2ee7a7d9ed963b84f5d665
user <sami.elnakadi@gmail.com>
2019-05-03 10:36:43 +0200

    happy drinl

---
+++ happydrink/src/components/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import logo from '../assets/logo.svg';
+import '../css/App.css';
+import {establishments} from './establishments/fixture.js';
+
+class App extends Component {
+
+  render() {
+
+
+    const listEstablishment = establishments.map( (establishment) => {
+        return (
+            // L'attribut "key" permet à React d'identifier les éléments.
+            // Cela améliore les performances lors de l'ajout,
+            // la modification et la suppression d'un élément.
+            <li
+                key = { establishment.id }
+                className = 'establishment'
+            >
+                <h3>{ establishment.name }</h3>
+
+                { establishment.description }
+
+            </li>
+        )
+    })
+
+    return (
+        <div className="App">
+            <div className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h2>Welcome to { this.props.title }</h2>
+            </div>
+            <div className="App-intro">
+                { listEstablishment }
+            </div>
+
+        </div>
+    )
+}
+}
+export default App;
---
+++ happydrink/src/components/establishments/fixture.js
@@ -0,0 +1,22 @@
+export const establishments = [
+    {
+        id          : "0890786GH",
+        name        : "Biberium",
+        description : "Un super bar karaoké"
+    },
+    {
+        id          : "0890786GD",
+        name        : "Brew Dogs",
+        description : "Un super bar à bière"
+    },
+    {
+        id          : "MJLMH0389",
+        name        : "Batonier",
+        description : "Un super bar de fin de soirée"
+    },
+    {
+        id          : "MJLMH0390",
+        name        : "BarAccouda",
+        description : "Un super bar de plein mort"
+    }
+]
