c9ac254b0abf9fe0d225ed86429c07688425eca7
user <sami.elnakadi@gmail.com>
2019-05-28 16:16:26 +0200

    done

--- OnePages/fullonepage/src/More.js
+++ OnePages/fullonepage/src/More.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import pic02 from './images/pic02.jpg';
+import pic03 from './images/pic03.jpg';
+import pic04 from './images/pic04.jpg';
 
 export default class More extends Component {
 
@@ -14,7 +17,7 @@ export default class More extends Component {
 					<p>Ante nunc accumsan et aclacus nascetur ac ante amet sapien sed.</p>
 					<div className="row gtr-150">
 						<div className="col-4 col-12-medium">
-							<span className="image fit"><img src="images/pic02.jpg" alt="" /></span>
+							<span className="image fit"><img src={pic02} alt="" /></span>
 							<h3>Magna feugiat lorem</h3>
 							<p>Adipiscing a commodo ante nunc magna lorem et interdum mi ante nunc lobortis non amet vis sed volutpat et nascetur.</p>
 							<ul className="actions special">
@@ -22,7 +25,7 @@ export default class More extends Component {
 							</ul>
 						</div>
 						<div className="col-4 col-12-medium">
-							<span className="image fit"><img src="images/pic03.jpg" alt="" /></span>
+							<span className="image fit"><img src={pic03} alt="" /></span>
 							<h3>Magna feugiat lorem</h3>
 							<p>Adipiscing a commodo ante nunc magna lorem et interdum mi ante nunc lobortis non amet vis sed volutpat et nascetur.</p>
 							<ul className="actions special">
@@ -30,7 +33,7 @@ export default class More extends Component {
 							</ul>
 						</div>
 						<div className="col-4 col-12-medium">
-							<span className="image fit"><img src="images/pic04.jpg" alt="" /></span>
+							<span className="image fit"><img src={pic04} alt="" /></span>
 							<h3>Magna feugiat lorem</h3>
 							<p>Adipiscing a commodo ante nunc magna lorem et interdum mi ante nunc lobortis non amet vis sed volutpat et nascetur.</p>
 							<ul className="actions special">
--- OnePages/fullonepage/src/Poser.js
+++ OnePages/fullonepage/src/Poser.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import pic01 from './images/pic01.jpg';
 
 export default class Poser extends Component {
 
@@ -17,7 +18,7 @@ export default class Poser extends Component {
 							<p>Adipiscing a commodo ante nunc accumsan et interdum mi ante adipiscing. A nunc lobortis non nisl amet vis sed volutpat aclacus nascetur ac non. Lorem curae et ante amet sapien sed tempus adipiscing id accumsan.</p>
 						</div>
 						<div className="col-6 col-12-medium imp-medium">
-							<span className="image fit"><img src="images/pic01.jpg" alt="" /></span>
+							<span className="image fit"><img src={pic01} alt="" /></span>
 						</div>
 					</div>
 				</div>
